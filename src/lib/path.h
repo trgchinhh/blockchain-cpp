64936ae16cb327c6386deec3ae0858446addf2c0
Trường Chinh <chinhcuber@gmail.com>
2026-07-24 08:32:35 +0700

    Update and rename path.hpp to path.h

--- src/lib/path.h
+++ src/lib/path.h
@@ -1,7 +1,7 @@
-#ifndef PATH_HPP
-#define PATH_HPP
+#ifndef PATH_H
+#define PATH_H
 
-#include "header.hpp"
+#include "header.h"
 
 const string path_data        = "..\\data\\"; 
 const string path_publickeys  = "..\\data\\public_keys.json";
